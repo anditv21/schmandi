d5c7f2c55e525cfe0933a503c085e1afd8819a5f
Andreas <anditv21@icloud.com>
2023-03-09 18:57:16 +0100

    🔧 Improved fact cmd
    Added ability to make fact visible for everyone

--- cogs/fun/fun.py
+++ cogs/fun/fun.py
@@ -56,42 +56,45 @@ class Fun(commands.Cog):
 
     @app_commands.command(name="fact", description="Shows you a useless fact")
     @app_commands.describe(language="In which language should your useless fact be shown?")
-    async def fact(self, interaction: discord.Interaction, language: Literal["English", "German"] = "English"):
+    async def fact(self, interaction: discord.Interaction, language: Literal["English", "German"] = "English", visibility: Literal["true", "false"] = "false"):
         try:
             # Mapping of languages to codes for the API
             language_codes = {"English": "en", "German": "de"}
+            
+            visibility = {"true": True, "false": False}.get(visibility, False)
+            
             code = language_codes.get(language)
             if not code:
-                await interaction.response.send_message("Sorry, that language is not supported.", ephemeral=True)
+                code = "en"
                 return
-
+            
             url = f"https://uselessfacts.jsph.pl/random.json?language={code}"
-            factembed = discord.Embed(timestamp=datetime.now(), color=discord.Color.dark_red(
-            )).set_footer(text=interaction.guild.name, icon_url=interaction.guild.icon.url)
+            factembed = discord.Embed(timestamp=datetime.now(), color=discord.Color.dark_red()).set_footer(text=interaction.guild.name, icon_url=interaction.guild.icon.url)
 
             try:
                 # Make a GET request to the API and check the response status code
                 response = requests.get(url)
-                if response.status_code != 200:
-                    factembed.add_field(name="Error Code:",
-                                        value=response.status_code)
-                    await interaction.response.send_message(embed=factembed, ephemeral=True)
-                    return
+                response.raise_for_status()  # Raise an exception for non-200 status codes
                 
                 # Parse the JSON response and get the useless fact
                 data = response.json()
                 fact = data["text"]
                 factembed.add_field(name="Useless Fact:", value=fact)
-                await interaction.response.send_message(embed=factembed, ephemeral=True)
+                
+                ephemeral = visibility == False  # Set ephemeral to True only if visibility is "false"
+                
+                await interaction.response.send_message(embed=factembed, ephemeral=ephemeral)
+        
             except requests.exceptions.RequestException as e:
                 factembed.add_field(name="Error:", value=str(e))
                 await interaction.response.send_message(embed=factembed, ephemeral=True)
             except ValueError:
-                factembed.add_field(
-                    name="Error:", value="Failed to parse response as JSON.")
+                factembed.add_field(name="Error:", value="Failed to parse response as JSON.")
                 await interaction.response.send_message(embed=factembed, ephemeral=True)
         except Exception as e:
             print(e)
+
+
             
 async def setup(bot):
     await bot.add_cog(Fun(bot))
