6cf71d7af5e1969432ccab4b47dc9d52d4161acc
Andreas <andi@anditv.dev>
2024-01-25 14:48:18 +0100

    ✅ Added readme link to config helper

--- helpers/config.py
+++ helpers/config.py
@@ -9,17 +9,21 @@ sys.dont_write_bytecode = True
 time = datetime.now().strftime('%d.%m.%Y %H:%M:%S')
 
 def check_config():
+    url= "https://github.com/anditv21/schmandi/blob/main/README.md"
     if not os.path.exists("config.json"):
         if os.path.exists("example.config.json"):
             print_failure_message("Please rename example.config.json to config.json and follow the setup instructions from the README file.")
+            os.system(f"start {url}")
             sys.exit()
         else:
             print_failure_message("config.json is missing. Please follow the setup instructions from the README file.")
+            os.system(f"start {url}")
             sys.exit()
     else:
         token = get_config_value("token")
         if not token:
             print_failure_message("Token is missing from config.json. Please follow the setup instructions from the README file.")
+            os.system(f"start {url}")
             sys.exit()
 
 def get_config_value(key: str) -> str:
